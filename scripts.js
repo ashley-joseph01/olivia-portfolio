1431bcda6dd2291cc70c6cd1548253bc1f7a55a1
ashleyjoseph021 <ashleyjoseph01@proton.me>
2026-02-12 16:47:25 -0700

    worked on nav

---
+++ scripts.js
@@ -0,0 +1,11 @@
+console.log("JS starting ...")
+
+/* Open */
+function openNav() {
+  document.getElementById("navWindow").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("navWindow").style.height = "0%";
+}
