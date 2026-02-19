ee5e1149da980bdf9f2428ae4de0f45fb3437a15
ashleyjoseph021 <ashleyjoseph01@proton.me>
2026-02-19 15:33:47 -0700

    worked on pages
    missing nav

--- scripts.js
+++ scripts.js
@@ -1,11 +1,2 @@
 console.log("JS starting ...")
 
-/* Open */
-function openNav() {
-  document.getElementById("navWindow").style.height = "100%";
-}
-
-/* Close */
-function closeNav() {
-  document.getElementById("navWindow").style.height = "0%";
-}
